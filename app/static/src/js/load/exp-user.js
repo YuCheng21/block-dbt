7f9cc0e4c744fd7b8c741c7dc34bcde5bb63988c
YuCheng <fyc621@gmail.com>
2023-02-10 14:46:19 +0800

    add: exp item "state" #20230210004

--- app/static/src/js/load/exp-user.js
+++ app/static/src/js/load/exp-user.js
@@ -1,11 +1,13 @@
 function fetch_data() {
     var rows = []
+    var state = ['已完成', '進行中', '等待中']
     for (var i = 0; i < 10; i++) {
         rows.push({
             id: 'N00' + i,
-            name: '實驗名稱',
-            member: '研究人員',
-            content: '實驗內容',
+            name: '實驗名稱' + i,
+            member: '研究人員' + i,
+            content: '實驗內容' + i,
+            state: state[Math.floor(Math.random() * state.length)],
             action: `
             <a href="/exp/update/N00${i}"
                class="btn btn-primary text-white">編輯</a>
