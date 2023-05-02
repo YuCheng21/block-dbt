f7181ec67d89642e6e02496cd0e9ce981e85cd8f
YuCheng <fyc621@gmail.com>
2023-05-02 14:55:55 +0800

    adjust: new topic and load default data #20230502005

--- app/static/src/js/exp/update.js
+++ app/static/src/js/exp/update.js
@@ -29,14 +29,43 @@ function fetch_data_SA() {
     }
     return rows
 }
+function load_table(data) {
+    const mc = [];
+    const sa = [];
+    Object.values(data).forEach((item) => {
+        if (item._type === 'choose') {
+            mc.push({
+                multipleChoice: `${item._topic}`,
+                maxScore: `${item._scale}`,
+                MCAction: `
+                <button class="btn btn-primary w-50 MCUpdate">編輯</button>
+                <button class="btn btn-danger w-50 MCDelete">刪除</button>
+                `
+            })
+        }
+        if (item._type === 'filling') {
+            sa.push({
+                shortAnswer: `${item._topic}`,
+                SAAction: `
+                <button class="btn btn-primary w-50 MCUpdate">編輯</button>
+                <button class="btn btn-danger w-50 MCDelete">刪除</button>
+                `
+            })
+        }
+    });
+    $('#MCTable').bootstrapTable('load', mc)
+    $('#SATable').bootstrapTable('load', sa)
+}
 
 let MCTable = document.querySelector('#MCTable')
 let SATable = document.querySelector('#SATable')
 if (MCTable && SATable && server.endpoint === 'exp.update') {
     document.addEventListener("DOMContentLoaded", function () {
-        utils.fetch_data(server.url.exp.index, server.basic_auth).then(data => {
-            $('#MCTable').bootstrapTable('load', fetch_data_MC())
-            $('#SATable').bootstrapTable('load', fetch_data_SA())
+        const body = {"scaddress": page.id};
+        utils.fetch_data(server.url.topic.index, server.basic_auth, 'POST', body).then(data => {
+            load_table(data)
+            // $('#MCTable').bootstrapTable('load', fetch_data_MC())
+            // $('#SATable').bootstrapTable('load', fetch_data_SA())
         })
     })
 }
