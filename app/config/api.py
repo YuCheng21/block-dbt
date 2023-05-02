3dc882f7fd5a2ba6179c5f42d9b171573a4d6833
YuCheng <fyc621@gmail.com>
2023-05-02 14:50:24 +0800

    add: api list #20230502001

--- app/config/api.py
+++ app/config/api.py
@@ -5,6 +5,11 @@ from app.config.base import settings
 class Exp(BaseModel):
     index: HttpUrl = f'{settings.api_url}/explist'
     store: HttpUrl = f'{settings.api_url}/newexp'
+    start: HttpUrl = f'{settings.api_url}/startExp'
+    sign_up: HttpUrl = f'{settings.api_url}/expRegister'
+    number: HttpUrl = f'{settings.api_url}/getMember'
+    submit: HttpUrl = f'{settings.api_url}/fillQuestion'
+    data: HttpUrl = f'{settings.api_url}/getResult'
 
 
 class User(BaseModel):
@@ -12,10 +17,19 @@ class User(BaseModel):
     sign_up: HttpUrl = f'{settings.api_url}/register'
 
 
+class Topic(BaseModel):
+    index: HttpUrl = f'{settings.api_url}/getQuestionaire'
+    store_mc: HttpUrl = f'{settings.api_url}/setNewOptions'
+    destroy: HttpUrl = f'{settings.api_url}/deleteOptions'
+    store_sa: HttpUrl = f'{settings.api_url}/setNewFillinOptions'
+    submit: HttpUrl = f'{settings.api_url}/checkQuestionaire'
+
+
 class Url(BaseModel):
     base: HttpUrl = f'{settings.api_url}'
     exp = Exp()
     user = User()
+    topic = Topic()
 
 
 url = Url()
