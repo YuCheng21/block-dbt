3d69a57fde89988d83e9ef39a8893469a730bc0c
YuCheng <fyc621@gmail.com>
2023-08-24 11:20:06 +0800

    adjust: rename js file #20230824005

--- app/static/src/js/config/state.js
+++ app/static/src/js/config/state.js
@@ -1,8 +1,8 @@
 export const state = {
-    '0': 's-form',
-    '1': 's-auth',
-    '2': 's-exp',
-    '3': 's-sub',
-    '4': 's-run',
-    '5': 's-over'
+    '0': 's-form',  // build
+    '1': 's-auth',  // auth
+    '2': 's-exp',  // experiment
+    '3': 's-sub',  // subject
+    '4': 's-run',  // running
+    '5': 's-over',  // finish
 }
