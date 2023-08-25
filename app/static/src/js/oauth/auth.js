e76a189414d3f730c7317669f9474ed58e645071
YuCheng <fyc621@gmail.com>
2023-08-25 15:41:26 +0800

    adjust: check exp state to setting action layout #20230825023

--- app/static/src/js/oauth/auth.js
+++ app/static/src/js/oauth/auth.js
@@ -24,18 +24,25 @@ function load_table(data) {
             member: utils.component_member(item._Researchers_name),
             content: utils.component_content(item._content),
             state: utils.component_state(item._status),
-            action: component_action(item._address)
+            action: component_action(item._address, item._status)
         })
     });
     $('#expTable').bootstrapTable('load', rows)
 }
 
-function component_action(id) {
+function component_action(id, code) {
     let href = utils.route2url(server.route.oauth.authenticate)
+    let active = `
+        <button type="submit" class="btn btn-primary w-100 setLoading">驗證</button> 
+    `
+    let disabled = `
+        <button type="submit" class="btn btn-close w-100 disabled"></button> 
+    `
+    let button = (['1'].includes(code) ? active : disabled)
     return `
         <form action="${href}" method="post" enctype="application/x-www-form-urlencoded">
             <input type="hidden" name="id" value="${id}">
-            <button type="submit" class="btn btn-primary w-100 setLoading">驗證</button>           
+            ${button}     
         </form>
     `
 }
