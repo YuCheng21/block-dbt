afbdc063681470950825f98db3cb9fa94d5f5d9e
YuCheng <fyc621@gmail.com>
2023-09-06 03:44:43 +0800

    add: exp information on exp.private.experiment page #20230906012

--- app/static/src/js/exp/private/experiment.js
+++ app/static/src/js/exp/private/experiment.js
@@ -3,6 +3,16 @@ import {endpoint} from "@static/src/js/config/endpoint";
 
 
 if (server.endpoint === endpoint.exp.private.experiment) {
+    document.addEventListener("DOMContentLoaded", function () {
+        const body = {"scaddress": page.id};
+        utils.fetch_data(server.url.exp.index, server.basic_auth).then(data => {
+            utils.load_info(data)
+        })
+        utils.fetch_data(server.url.topic.index, server.basic_auth, 'POST', body).then(data => {
+            utils.load_topic(data)
+        })
+    })
+
     let expList = document.querySelector("#expList")
     let contentSend = document.querySelector('#contentSend')
     contentSend.addEventListener('click', function () {
